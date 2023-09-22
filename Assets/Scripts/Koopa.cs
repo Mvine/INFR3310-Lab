5452525869e884424b3a3ee46ac41172e494c35c
Marcus Vine <marcus.vine@uoit.net>
2023-09-22 12:01:09 -0400

    coroutine nonsense

---
+++ Assets/Scripts/Koopa.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Koopa : Enemy
+{
+    public enum KoopaShellState
+    {
+        shelled,
+        unShelled
+    }
+
+    KoopaShellState shellState = KoopaShellState.shelled;
+
+    public override IEnumerator Wander()
+    {
+        return base.Wander();
+
+    }
+    public override void TakeDamage()
+    {
+        base.Die();
+    }
+
+    public override void DropLoot()
+    {
+        base.DropLoot();
+    }
+
+    public override void GroundCheck()
+    {
+        base.GroundCheck();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
